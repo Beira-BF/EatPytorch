6a56ea16d9f2219e725c57dc2ce6fdfcdb75ff04
baifang <baifang@stonewise.cn>
2020-11-27 21:34:19 +0800

    part towday

---
+++ twoday.py
@@ -0,0 +1,93 @@
+import os
+import datetime
+
+def printbar():
+    nowtime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print("\n"+"=========="*8 + "%s"%nowtime)
+
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
+# 一、准备数据
+# 在Pytorch中构建图片数据管道通常有三种方法
+# 第一种使用torchvision中的datasets.ImageFolder来读取图片然后用DataLoader来并行加载。
+# 第二种是通过继承torch.utils.data.Dataset实现用户自定义读取逻辑然后用DataLoader来并行加载。
+# 第三种方法是读取用户自定义数据集的通用方法，既可以读取图片数据集，也可以读取文本数据集。
+# 在此使用第一种方法：
+
+import torch
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, datasets
+
+transform_train = transforms.Compose([transforms.ToTensor()])
+transform_valid = transforms.Compose([transforms.ToTensor()])
+
+ds_train = datasets.ImageFolder("./data/cifar2/train/",
+                                transform=transform_train, target_transform=lambda t:torch.tensor([t]).float())
+ds_valid = datasets.ImageFolder("./data/cifar2/test/",
+                                transform=transform_valid, target_transform=lambda t:torch.tensor([t]).float())
+print(ds_train.class_to_idx)
+
+dl_train = DataLoader(ds_train, batch_size=50, shuffle=True, num_workers=3)
+dl_valid = DataLoader(ds_valid, batch_size=50, shuffle=True, num_workers=3)
+
+# 查看部分样本
+from matplotlib import pyplot as plt
+
+plt.figure(figsize=(8,8))
+for i in range(9):
+    img,label = ds_train[i]
+    img = img.permute(1,2,0)
+    ax=plt.subplot(3,3,i+1)
+    ax.imshow(img.numpy())
+    ax.set_title("label = %d"%label.item())
+    ax.set_xticks([])
+    ax.set_yticks([])
+plt.show()
+
+# Pytorch的图片默认顺序是Batch, Channel, Width, Height
+for x,y in dl_train:
+    print(x.shape, y.shape)
+    break
+
+# 二、定义模型
+# Pytorch通常有三种方式构建模型：使用nn.Sequential按层顺序构建模型，继承nn.Module基类构建自定义模型，继承nn.Module基类构建
+# 模型并辅助应用模型容器（nn.Sequential, nn.ModuleList, nn.ModuleDict）进行封装。
+# 此处选择通过继承nn.Module基类构建自定义模型。
+
+# 测试AdaptiveMaxPool2d的效果
+pool = nn.AdaptiveMaxPool2d((1,1))
+t = torch.randn(10,8,32,32)
+print(pool(t).shape)
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5)
+        self.dropout = nn.Dropout2d(p=0.1)
+        self.adaptive_pool = nn.AdaptiveMaxPool2d((1,1))
+        self.flatten = nn.Flatten()
+        self.linear1 = nn.Linear(64,32)
+        self.relu = nn.ReLU()
+        self.linear2 = nn.Linear(32,1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self,x):
+        x = self.conv1(x)
+        x = self.pool(x)
+        x = self.conv2(x)
+        x = self.pool(x)
+        x = self.dropout(x)
+        x = self.adaptive_pool(x)
+        x = self.flatten(x)
+        x = self.linear1(x)
+        x = self.relu(x)
+        x = self.linear2(x)
+        y = self.sigmoid(x)
+        return y
+
+net = Net()
+print(net)
+
