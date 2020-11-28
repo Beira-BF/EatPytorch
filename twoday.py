0ca58203a5fbf6bd4e6be264acf12d310761e4ef
baifang <baifang@stonewise.cn>
2020-11-29 00:27:31 +0800

    twoday full

--- twoday.py
+++ twoday.py
@@ -91,5 +91,134 @@ class Net(nn.Module):
 net = Net()
 print(net)
 
+import pandas as pd
+from sklearn.metrics import roc_auc_score
 
+model = net
+model.optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+model.loss_func = lambda y_pred, y_true: roc_auc_score(y_true.data.numpy(),y_pred.data.numpy())
+model.metric_name = "auc"
 
+def train_step(model, features, labels):
+
+    # 训练模式，dropout层发生作用
+    model.train()
+
+    # 梯度清零
+    model.optimizer.zero_grad()
+
+    # 正向传播求损失
+    predictions = model(features)
+    loss = model.loss_func(predictions, labels)
+    metric = model.metric_func(predictions, labels)
+
+    # 反向传播求梯度
+    loss.backward()
+    model.optimizer.step()
+
+    return loss.item(), metric.item()
+
+def valid_step(model, features, labels):
+
+    # 预测模式，dropout层不发生作用
+    model.eval()
+    # 关闭梯度计算
+    with torch.no_grad():
+        predictions = model(features)
+        loss = model.loss_func(predictions, labels)
+        metric = model.metric_func(predictions, labels)
+
+    return loss.item(), metric.item()
+
+# 测试train_step效果
+features, labels = next(iter(dl_train))
+train_step(model, features, labels)
+
+def train_model(model, epochs, dl_train, dl_valid, log_step_freq):
+
+    metric_name = model.metric_name
+    dfhistory = pd.DataFrame(columns = ["epoch", "loss", metric_name, "val_loss", "val_"+metric_name])
+    print("Start Training...")
+    nowtime = datetime.datetime.now().striftime('%Y-%m-%d %H:%M:%S')
+    print("============"*8 + "%s"%nowtime)
+
+    for epoch in range(1, epoch+1):
+        # 1.训练循环-----------------------------------------
+        loss_sum = 0.0
+        metric_sum = 0.0
+        step = 1
+
+        for step, (features, labels) in enumerate(dl_train, 1):
+
+            loss, metric = train_step(model, features, labels)
+            # 打印batch级别日志
+            loss_sum += loss
+            metric_sun += metric
+            if step%log_step_freq == 0:
+                print(("[step = %d] loss: %.3f, "+metric_name+": %.3f")%(step, loss_sum/step, metric_sum/step))
+
+        # 2. 验证循环------------------------------------------
+        val_loss_sum = 0.0
+        val_metric_sum = 0.0
+        val_step = 1
+
+        for val_step, (features, labels) in enumerate(dl_valid, 1):
+            val_loss, val_metric = valid_step(model, features, labels)
+            val_loss_sum += val_loss
+            val_metric_sum += val_metric
+
+        # 3. 记录日志------------------------------------------
+        info = (epoch, loss_sum/step, metric_sum/step, val_loss_sum/val_step, val_metric_sum/val_step)
+        dfhistory.loc[epoch -1] = info
+
+        # 打印epoch级别日志
+        print(("\nEPOCH = %d, loss = %.3f,"+metric_name+" = %.3f, val_loss = %.3f, "+"val+"+metirc_name+" = %.3f")%info)
+        nowtime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print("\n"+"=========="*8 + "%s"%nowtime)
+
+    print("Finished Training...")
+
+    return dfhistory
+
+import matplotlib.pylot as plt
+
+def plot_metric(dfhistory, metric):
+    train_metrics = dfhistory[metric]
+    val_metrics = dfhistory['val_'+metric]
+    epochs = range(1, len(train_metrics)+1)
+    plt.plot(epochs, train_metrics, 'bo--')
+    plt.plot(epochs, val_metrics, 'ro-')
+    plt.title('Training and validation '+ metric)
+    plt.xlabel("Epochs")
+    plt.ylabel(metric)
+    plt.legend(["train_"+metric, 'val_'+metric])
+    plt.show()
+
+plot_metric(dfhistory, "loss")
+
+plot_metric(dfhistory, "auc")
+
+def predict(model, dl):
+    model.eval()
+    with torch.no_grad():
+        result = torch.cat([model.forward(t[0]) for t in dl])
+    return (result.data)
+
+# 预测概率
+y_pred_probs = predict(model, dl_valid)
+print(y_pred_probs)
+
+# 预测类别
+y_pred = torch.where(y_pred_probs>0.5, torch.ones_like(y_pred_probs), torch.zeros_like(y_pred_probs))
+print(y_pred)
+
+# 保存模型
+print(model.state_dict().keys())
+
+# 保存模型参数
+torch.save(model.state_dict(), "./data/model_parameter_cifar2.pkl")
+
+net_clone = Net()
+net_clone.load_state_dict(torch.load("./data/model_parameter_cifar2.pkl"))
+
+predict(net_clone, dl_valid)
